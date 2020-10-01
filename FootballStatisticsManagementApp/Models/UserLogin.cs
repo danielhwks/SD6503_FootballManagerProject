d90da8e6827d39dcfd87612075ef897f06a95095
qiangnz <qiang.z@outlook.com>
2020-10-02 11:12:35 +1300

    create UserLogin.cs

---
+++ FootballStatisticsManagementApp/Models/UserLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserLogin.Models
+{
+    public class UserLogin
+    {
+        [Required]
+        [RegularExpression("^[a-z]+$",ErrorMessage = "Please enter lowercase characters")]
+        [StringLength(5, ErrorMessage = "Please enter no more than 5 characters")]
+        public string username { get; set; }
+
+        [Required]
+        [RegularExpression("^[a-z]+$", ErrorMessage = "Please enter lowercase characters")]
+        [StringLength(5, ErrorMessage = "Please enter no more than 5 characters")]
+        public string password { get; set; }
+
+
+    }
+}
