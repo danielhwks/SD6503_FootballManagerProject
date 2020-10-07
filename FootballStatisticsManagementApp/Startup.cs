7e25f4447472d21899af0964b5cf585804d11163
Dan Hawkes <dan.hawkes@soit.local>
2020-10-08 12:46:17 +1300

    Changed login timeout from 10 seconds to 1 hour

--- FootballStatisticsManagementApp/Startup.cs
+++ FootballStatisticsManagementApp/Startup.cs
@@ -37,7 +37,7 @@ namespace FootballStatisticsManagementApp
 
             services.AddSession(options =>
             {
-                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.IdleTimeout = TimeSpan.FromSeconds(3600);
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
