7a6e9eae368b23d8f3cee8b5e674aaa86d18802c
Alen Jose <Alen.Jose@soit.local>
2020-10-08 15:02:10 +1300

    Validation for Dates in Matches

--- FootballStatisticsManagementApp/Models/League.cs
+++ FootballStatisticsManagementApp/Models/League.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FootballStatisticsManagementApp.Models
 {
@@ -10,7 +11,13 @@ namespace FootballStatisticsManagementApp.Models
             Match = new HashSet<Match>();
         }
 
+        [Required]
+        [RegularExpression("^[0-9]+$",ErrorMessage = "Must input numbers in field")]
         public int LeagueId { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "Incorrect data in field")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "Year must be four digits long")]
         public string Year { get; set; }
 
         public ICollection<Match> Match { get; set; }
--- FootballStatisticsManagementApp/Models/Match.cs
+++ FootballStatisticsManagementApp/Models/Match.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FootballStatisticsManagementApp.Models
 {
@@ -13,6 +14,11 @@ namespace FootballStatisticsManagementApp.Models
         public int MatchId { get; set; }
         public string Location { get; set; }
         public string Date { get; set; }
+
+        [Required]
+        [RegularExpression("^([0-2][0-9]|(3)[0-1])(\\/)(((0)[0-9])|((1)[0-2]))(\\/)\\d{4}$", 
+            ErrorMessage = "Date should be in dd-mm-yyyy format")]
+        
         public int LeagueId { get; set; }
         public int HomeTeamId { get; set; }
         public int AwayTeamId { get; set; }
