6b6c9705ca8f52dc7783245b26de8f08b3b3dbbd
Dan Hawkes <dan.hawkes@soit.local>
2020-10-09 11:00:43 +1300

    Added unit tests for login controller

---
+++ FSMATests/LoginControllerUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using FootballStatisticsManagementApp.Controllers;
+using FootballStatisticsManagementApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Tests
+{
+    public class LoginControllerUnitTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ValidResponseIndex()
+        {
+            LoginController controller = new LoginController();
+            IActionResult result = controller.Index() as IActionResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
