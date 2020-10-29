694c6e4e81fb329c0ffca81daa031470d844aa6a
Dan Hawkes <dan.hawkes@soit.local>
2020-10-29 13:28:04 +1300

    Added League search

--- FootballStatisticsManagementApp/Controllers/LeaguesController.cs
+++ FootballStatisticsManagementApp/Controllers/LeaguesController.cs
@@ -19,10 +19,13 @@ namespace FootballStatisticsManagementApp.Controllers
         }
 
         // GET: Leagues
-        public async Task<IActionResult> Index(string sortParam)
+        public async Task<IActionResult> Index(string sortParam, string searchParam)
         {
             var leagues = from l in _context.League select l;
-
+            if (!String.IsNullOrEmpty(searchParam))
+            {
+                leagues = leagues.Where(l => l.Year.Contains(searchParam));
+            }
             switch (sortParam)
             {
                 case "year_asc":
