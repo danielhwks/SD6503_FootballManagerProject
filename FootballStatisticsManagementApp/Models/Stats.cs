fdbbf29c0088806a54400ce49872ba7fc8764539
Alen Jose <Alen.Jose@soit.local>
2020-10-30 12:50:16 +1300

    Added validation to stats page

--- FootballStatisticsManagementApp/Models/Stats.cs
+++ FootballStatisticsManagementApp/Models/Stats.cs
@@ -9,15 +9,15 @@ namespace FootballStatisticsManagementApp.Models
         public int StatsId { get; set; }
 
         [Required]
-        [RegularExpression("^[0-9]+$", ErrorMessage = "Please enter a number")]
+        [RegularExpression("^[0-9]\\d*$", ErrorMessage = "Please enter a positive number")]
         public int? Goals { get; set; }
 
         [Required]
-        [RegularExpression("^[0-9]+$", ErrorMessage = "Please enter a number")]
+        [RegularExpression("^[0-9]\\d*$", ErrorMessage = "Please enter a positive number")]
         public int Assists { get; set; }
 
         [Required]
-        [RegularExpression("^[0-9]+$", ErrorMessage = "Please enter a number")]
+        [RegularExpression("^[0-9]\\d*$", ErrorMessage = "Please enter a positive number")]
         public int Saves { get; set; }
         public int MatchId { get; set; }
         public int PlayerId { get; set; }
--- FootballStatisticsManagementApp/Models/Team.cs
+++ FootballStatisticsManagementApp/Models/Team.cs
@@ -16,11 +16,11 @@ namespace FootballStatisticsManagementApp.Models
         public int TeamId { get; set; }
 
         [Required]
-        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Name can only include letters")]
+        [RegularExpression("^[a-zA-Z\\s]+$", ErrorMessage = "Name can only include letters")]
         public string Name { get; set; }
 
         [Required]
-        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Location can only include letters")]
+        [RegularExpression("^[a-zA-Z\\s]+$", ErrorMessage = "Location can only include letters")]
         public string Location { get; set; }
 
         public ICollection<Match> MatchAwayTeam { get; set; }
