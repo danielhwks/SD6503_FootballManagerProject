528124c88f54079d3f5fcfa8bee6303fdde20034
Dan Hawkes <dan.hawkes@soit.local>
2020-10-29 16:27:04 +1300

    Added top player stats to home page

--- FootballStatisticsManagementApp/Controllers/HomeController.cs
+++ FootballStatisticsManagementApp/Controllers/HomeController.cs
@@ -6,16 +6,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FootballStatisticsManagementApp.Models;
 using FootballStatisticsManagementApp.Utilities;
+using Microsoft.EntityFrameworkCore;
 
 namespace FootballStatisticsManagementApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HSD6503_ProjectSD6503_Project_DBDatabaseFSMDBmdfContext _context;
+
+        public HomeController(HSD6503_ProjectSD6503_Project_DBDatabaseFSMDBmdfContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             if (!LoginUtility.CheckAuthenticated(HttpContext.Session))
             {
                 return RedirectToAction("Index", "Login");
+            }
+
+            var _ = _context.Stats.ToList();
+            var _2 = _context.Team.ToList();
+            var stats = _context.Stats.Include(s => s.Player.Team)
+                .GroupBy(i => i.Player)
+                .Select(g => new { Player = g.Key, Team = g.Key.Team, Total = g.Sum(j => j.Goals) })
+                .OrderByDescending(g => g.Total).Take(3);
+
+            //ViewBag.topPlayers = stats.ToList();
+            List<(Player, Team, int)> realStats = new List<(Player, Team, int)>();
+            foreach (var stat in stats.ToList())
+            {
+                realStats.Add((stat.Player, stat.Team, (int)stat.Total));
+            }
+
+            ViewBag.topPlayers = realStats.ToList();
+
+            foreach (var stat in realStats)
+            {
+                Console.WriteLine(stat.Item1.Name);
+                Console.WriteLine(stat.Item1.Team.Name);
             }
 
             return View();
