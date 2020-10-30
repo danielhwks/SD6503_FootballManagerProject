d296c3d674619fdfb3762e937847e4b142627c02
Dan Hawkes <dan.hawkes@soit.local>
2020-10-30 15:33:09 +1300

    Added comments to Team unit tests

--- FSMATests/TeamControllerUnitTest.cs
+++ FSMATests/TeamControllerUnitTest.cs
@@ -14,6 +14,8 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+            // Creates a new database context in memory for each test, this ensures that nothing is kept
+            //  between tests that could interfere with the testing or give incorrent results.
             var options = new DbContextOptionsBuilder<HSD6503_ProjectSD6503_Project_DBDatabaseFSMDBmdfContext>()
                 .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
             databaseContext = new HSD6503_ProjectSD6503_Project_DBDatabaseFSMDBmdfContext(options);
@@ -23,48 +25,73 @@ namespace Tests
         [Test]
         public void ValidResponseIndex()
         {
+            // Create the Team controller
             TeamsController controller = new TeamsController(databaseContext);
+
+            // Call the Index page and wait for a response
             var resultTask = controller.Index();
             resultTask.Wait();
             IActionResult result = resultTask.Result as IActionResult;
+
+            // Check that the controller returned a valid response
             Assert.IsNotNull(result);
         }
 
         [Test]
         public void ValidResponseDetails()
         {
+            // Create the Team controller
             TeamsController controller = new TeamsController(databaseContext);
+
+            // Call the Index page and wait for a response
             var resultTask = controller.Details(1);
             resultTask.Wait();
             IActionResult result = resultTask.Result as IActionResult;
+
+            // Check that the controller returned a valid response
             Assert.IsNotNull(result);
         }
 
         [Test]
         public void ValidResponseCreate()
         {
+            // Create the Team controller
             TeamsController controller = new TeamsController(databaseContext);
+
+            // Call the Index page
             IActionResult result = controller.Create() as IActionResult;
+
+            // Check that the controller returned a valid response
             Assert.IsNotNull(result);
         }
 
         [Test]
         public void ValidResponseEdit()
         {
+            // Create the Team controller
             TeamsController controller = new TeamsController(databaseContext);
+
+            // Call the Index page and wait for a response
             var resultTask = controller.Edit(1);
             resultTask.Wait();
             IActionResult result = resultTask.Result as IActionResult;
+
+            // Check that the controller returned a valid response
             Assert.IsNotNull(result);
         }
 
         [Test]
         public void ValidResponseDelete()
         {
+            // Create the Team controller
             TeamsController controller = new TeamsController(databaseContext);
+
+            // Call the Index page and wait for a response
             var resultTask = controller.Delete(1);
             resultTask.Wait();
             IActionResult result = resultTask.Result as IActionResult;
+
+            // Check that the controller returned a valid response
             Assert.IsNotNull(result);
         }
     }
